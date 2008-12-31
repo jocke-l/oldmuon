1ec798e9bb3c6e1e8a529d993bbb093e6f723971
Jocke <s@xyr.se>
2008-12-31 16:33:10 +0000

    Ny version av ui.c med den 'rätta' layouten
    git-svn-id: http://muon.googlecode.com/svn/trunk@38 99aa2080-d4e9-11dd-8179-53c65a4d6d95

--- ui.c
+++ ui.c
@@ -3,10 +3,24 @@
 void drawHud() {
 	SDL_Rect bg;
 
-	bg.x = 0;
-	bg.y = map.windh - (3 * 32);
-	bg.w = map.windw;
-	bg.h = 3 * 32;
+	bg.w = 40;
+	bg.h = 40;
+	bg.x = 4;
+	bg.y = map.windh - (40 + 4);
+
+	SDL_FillRect(context, &bg, SDL_MapRGB(context->format, 0x22, 0x22, 0x22));
+
+	int i;
+	for (i = 0; i < 4; i++) {
+		bg.x += 40 + 4;
+
+		SDL_FillRect(context, &bg, SDL_MapRGB(context->format, 0x22, 0x22, 0x22));
+	}
+
+	bg.w = 136;
+	bg.h = 136;
+	bg.x = 4;
+	bg.y = 4;
 
 	SDL_FillRect(context, &bg, SDL_MapRGB(context->format, 0x22, 0x22, 0x22));
 }
