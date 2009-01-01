b03f44b442ae67f77204cef1fd22aefef942b2ab
Chris <k@rdw.se>
2009-01-01 13:46:22 +0000

    fixat minimap, dock visas vyn som en fylld rektangel, kan inte hitta nan funktion for att rita rektangels outlines endast...
    git-svn-id: http://muon.googlecode.com/svn/trunk@39 99aa2080-d4e9-11dd-8179-53c65a4d6d95

--- ui.c
+++ ui.c
@@ -3,24 +3,53 @@
 void drawHud() {
 	SDL_Rect bg;
 
-	bg.w = 40;
-	bg.h = 40;
+	bg.w = 32;
+	bg.h = 32;
 	bg.x = 4;
-	bg.y = map.windh - (40 + 4);
+	bg.y = map.windh - (32 + 4);
 
 	SDL_FillRect(context, &bg, SDL_MapRGB(context->format, 0x22, 0x22, 0x22));
 
 	int i;
 	for (i = 0; i < 4; i++) {
-		bg.x += 40 + 4;
+		bg.x += 32 + 4;
 
 		SDL_FillRect(context, &bg, SDL_MapRGB(context->format, 0x22, 0x22, 0x22));
 	}
 
-	bg.w = 136;
-	bg.h = 136;
+	float scale_factor = 0;
+	if (map.width >= map.height) {
+		scale_factor = 128 / map.width;
+	} else {
+		scale_factor = 128 / map.height;
+	}
+
+	bg.w = map.width * scale_factor;
+	bg.h = map.height * scale_factor;
 	bg.x = 4;
 	bg.y = 4;
 
 	SDL_FillRect(context, &bg, SDL_MapRGB(context->format, 0x22, 0x22, 0x22));
+
+	for (i = 0; object_array[i].type != 0; i++) {
+		bg.x = 4 + object_array[i].x * scale_factor;
+		bg.y = 4 + object_array[i].y * scale_factor;
+		bg.w = 4;
+		bg.h = 4;
+
+/*
+		int r, g, b;
+		fixa färg här sen :D
+*/
+
+		SDL_FillRect(context, &bg, SDL_MapRGB(context->format, 0xFF, 0xFF, 0xFF));
+	}
+
+	bg.x = 4 + map.camx / 32 * scale_factor;
+	bg.y = 4 + map.camy / 32 * scale_factor;
+	bg.w = map.windw / 32 * scale_factor;
+	bg.h = map.windh / 32 * scale_factor;
+
+	SDL_FillRect(context, &bg, SDL_MapRGB(context->format, 0x77, 0x77, 0x77));
+
 }
